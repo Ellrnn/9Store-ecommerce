5cbcdee13f9cfb6a66dd61814ca2dd5b3fee5a4a
Ellrnn <rannamead58@gmail.com>
2024-11-26 09:46:56 -0300

    initial setup

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "images.tcdn.com.br",
+      "m.media-amazon.com",
+      "ae01.alicdn.com",
+      "auhekids.com.br",
+    ],
+  },
+};
+
+module.exports = nextConfig;
---
+++ tailwind.config.ts
@@ -0,0 +1,40 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: ["class"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        gray: {
+          900: "#1a202c",
+          800: "#2d3748", // Exemplo de cor personalizada
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "caret-blink": {
+          "0%,70%,100%": { opacity: "1" },
+          "20%,50%": { opacity: "0" },
+        },
+      },
+      animation: {
+        "caret-blink": "caret-blink 1.25s ease-out infinite",
+      },
+      boxShadow: {
+        custom: "-2px 2px 10px 0px rgba(0, 0, 0, 0.05)",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
